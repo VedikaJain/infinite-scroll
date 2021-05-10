0f9232d0780342115682a41f2242c2c0aafff92d
Vedika Jain <jain.vj.2@pg.com>
2021-05-11 01:29:08 +0530

    Removed unnecesary code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import { Login } from './Login';
 
 test('renders login page', () => {
   render(<App />);
